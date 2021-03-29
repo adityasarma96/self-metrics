25b0bd90fbe9392ac9b588906ccc40e9d08e6981
adityasarma96 <adityasarma96@gmail.com>
2021-03-29 19:14:21 +0530

    Initial Commit

---
+++ src/main/java/com/aditya/selfmetrics/SelfMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.selfmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfMetricsApplication.class, args);
+    }
+
+}
