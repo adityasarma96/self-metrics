bd9a14def09d558317b20c6e22a788c3466f4176
adityasarma96 <adityasarma96@gmail.com>
2021-05-04 16:50:13 +0530

    dummy commit

--- src/main/java/com/aditya/selfmetrics/controller/HelloResource.java
+++ src/main/java/com/aditya/selfmetrics/controller/HelloResource.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest.docker/hello")
+@RequestMapping("/rest/docker/hello")
 public class HelloResource {
 
     @GetMapping
     public String hello(){
         return "Hello World!";
+
+
+
     }
 }
