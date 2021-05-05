e1297222f4b7e2b52344efe1377cdca44a2caba3
adityasarma96 <adityasarma96@gmail.com>
2021-05-05 19:52:54 +0530

    updated hello controller

--- src/main/java/com/aditya/selfmetrics/controller/HelloResource.java
+++ src/main/java/com/aditya/selfmetrics/controller/HelloResource.java
@@ -1,15 +1,19 @@
 package com.aditya.selfmetrics.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/docker/hello")
 public class HelloResource {
 
     @GetMapping
     public String hello(){
         return "Hello World!";
+    }
+
+    @GetMapping("{id}")
+    public String helloPerson(@PathVariable  String id){
+        return "Hello " + id + "!";
     }
 }
