e8d0b771a06f53945c8dd1bb1a1eaa0d78a0d43b
adityasarma96 <adityasarma96@gmail.com>
2021-05-10 21:48:22 +0530

    use aws ecs uri credentials

--- src/main/java/com/aditya/selfmetrics/config/AwsClientConfig.java
+++ src/main/java/com/aditya/selfmetrics/config/AwsClientConfig.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.UriComponentsBuilder;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
@@ -19,9 +17,9 @@ public class AwsClientConfig {
 
     @Value("${aws.dynamodb.endpoint}")
     private String dynamoDbEndpoint;
-    @Value("${aws.accesskey}")
+    @Value("${aws.accessKeyId}")
     private String accessKey;
-    @Value("${aws.secretkey}")
+    @Value("${aws.secretKey}")
     private String secretKey;
     @Value("${aws.region}")
     private String region;
@@ -37,7 +35,7 @@ public class AwsClientConfig {
     public DynamoDbEnhancedClient dynamoDbEnhancedClient(){
         DynamoDbClient client = DynamoDbClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .endpointOverride(URI.create(dynamoDbEndpoint))
                 .build();
         return DynamoDbEnhancedClient.builder().dynamoDbClient(client).build();
