5f37dc826156b24b96b333f1a69ed2dbf41355f3
adityasarma96 <adityasarma96@gmail.com>
2021-05-15 19:57:15 +0530

    Removed unused beans from AwsClientConfig

--- src/main/java/com/aditya/selfmetrics/config/AwsClientConfig.java
+++ src/main/java/com/aditya/selfmetrics/config/AwsClientConfig.java
@@ -3,13 +3,11 @@ package com.aditya.selfmetrics.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.util.UriComponentsBuilder;
-import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-import javax.annotation.PostConstruct;
 import java.net.URI;
 
 @Configuration
@@ -17,22 +15,12 @@ public class AwsClientConfig {
 
     @Value("${aws.dynamodb.endpoint}")
     private String dynamoDbEndpoint;
-    @Value("${aws.accessKeyId}")
-    private String accessKey;
-    @Value("${aws.secretKey}")
-    private String secretKey;
+
     @Value("${aws.region}")
     private String region;
 
-    private AwsCredentials credentials;
-
-    @PostConstruct
-    public void createCredentials(){
-        credentials = AwsBasicCredentials.create(accessKey,secretKey);
-    }
-
     @Bean
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient(){
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient() {
         DynamoDbClient client = DynamoDbClient.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
